6037ba096b671584b0e70114bbf0ace6b6626260
jheidegg <jth3141@gmail.com>
2018-09-27 12:47:22 -0400

    changed stuff

--- src/BuggyUtilClient.java
+++ src/BuggyUtilClient.java
@@ -6,7 +6,9 @@ public class BuggyUtilClient {
         System.out.println("In: first = " + "\"Sean\"" +
                 ", last = " + "\"Flannery\"");
         System.out.println("Out: " +
-        StringUtils.makePurdueUsername("Sean", "Flannery"));
+
+                StringUtils.makePurdueUsername("Sean", "Flannery"));
+        //changes
     }
 }
 
