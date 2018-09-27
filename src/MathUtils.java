27fcdc5c7c7695a77c45ba443a2b404d0dfd413e
jheidegg <jth3141@gmail.com>
2018-09-27 13:05:18 -0400

    fixed error. now passes all test cases

--- src/MathUtils.java
+++ src/MathUtils.java
@@ -5,7 +5,7 @@ public class MathUtils {
      */
     public static double divideNumbers(int dividend, int divisor) {
         /* Do some casual division ... Super easy ... Shouldn't be any problems here */
-        double res = dividend / divisor;
+        double res = (double)dividend / (double)divisor;
         /* Return the DEFINITELY CORRECT result */
         return res;
     }
@@ -21,7 +21,7 @@ public class MathUtils {
         /* Determine if the exponent is negative */
         boolean isNegative = false;
         if (exp < 0) {
-            exp *= -1;
+            //exp *= -1;
             isNegative = true;
         }
 
@@ -35,11 +35,12 @@ public class MathUtils {
         /* If it's a negative exponent, we should invert it! */
         if (isNegative) {
             double dividedRes = divideNumbers(1, res);
-            return dividedRes;
+            //return dividedRes;
         }
         /* Otherwise, we are safe to just return the result */
         else {
-            return res;
+            //return res;
         }
+        return Math.pow(base,exp);
     }    
 }
